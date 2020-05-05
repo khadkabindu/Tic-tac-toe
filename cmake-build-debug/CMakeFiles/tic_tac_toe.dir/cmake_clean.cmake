fab9aff1d8b5aa9f2548b2827bd1fbfd3894f165
bindu <bindu@Rajeshs-MacBook-Pro.local>
2020-05-05 10:53:07 +0545

    final

---
+++ cmake-build-debug/CMakeFiles/tic_tac_toe.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tic_tac_toe.dir/main.c.o"
+  "tic_tac_toe"
+  "tic_tac_toe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tic_tac_toe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
